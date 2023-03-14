69c051eb1b0fb7f69939f2548166d98c4d6af77f
louiee15 <lejraee@hotmail.com>
2023-03-14 14:51:38 +0000

    login system done :D

--- UCLanARApp/Assets/Scripts/LoginHandler.cs
+++ UCLanARApp/Assets/Scripts/LoginHandler.cs
@@ -3,20 +3,32 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using System;
+using System.IO;
+using System.Linq;
 
 public class LoginHandler: MonoBehaviour
 {
     // SerializeField tag allows things to be seen and assigned in the editor, but keeps variables private
-    [SerializeField] private TMP_InputField emailInput;
+    [SerializeField] private TMP_InputField usernameInput;
     [SerializeField] private TMP_InputField passwordInput;
     [SerializeField] private Button loginButton;
     [SerializeField] private TMP_Text errorText;
     [SerializeField] private Button registerButton;
 
-
-    private string emailAddress;
+    private string username;
     private string password;
+
+    //save this data to be accessed by other scenes
+    public static int colourBlindSetting = 0;
+    public static bool dyslexicSetting = false;
+
+
+    private List<string> readUsernames = new List<string>();
+    private List<string> readPasswords = new List<string>();
+    private List<int> readColourBlindSettings = new List<int>();
+    private List<bool> readDyslexicSettings = new List<bool>();
+
 
     //Starts the screen in correct orientation
     private void Start()
@@ -24,6 +36,20 @@ public class LoginHandler: MonoBehaviour
         Screen.orientation = ScreenOrientation.Portrait;
         loginButton.onClick.AddListener(Login_Attempt); //when the login button is clicked
         registerButton.onClick.AddListener(Register);
+
+
+        string filePath = Application.dataPath + "/database.csv";
+        StreamReader reader = new StreamReader(filePath);
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            string[] values = line.Split(',');
+            readUsernames.Add(values[0]);
+            readPasswords.Add(values[1]);
+            readColourBlindSettings.Add(Convert.ToInt32(values[2]));
+            readDyslexicSettings.Add(Convert.ToBoolean(values[3]));
+        }
+        reader.Close();
     }
 
 
@@ -35,11 +61,11 @@ public class LoginHandler: MonoBehaviour
 
     private void Login_Attempt()
     {
-        emailAddress = emailInput.text;
+        username = usernameInput.text;
         password = passwordInput.text;
 
         // checks if username and password are valid
-        if (ValidateCredentials(emailAddress, password))
+        if (ValidateCredentials(username, password))
         {
             SceneManager.LoadScene("HomeScreen");
             // load next scene or do other actions here
@@ -47,15 +73,25 @@ public class LoginHandler: MonoBehaviour
         else
         {
             errorText.enabled = true;
-            // show error message or do other actions here
+            // show error message when invalid credentials inputted
         }
     }
 
     private bool ValidateCredentials(string username, string password)
     {
-        // replace this with user login
-        //Link up to database of user data
+        bool result = false;
 
-        return username == "admin" && password == "password";
+        for (int i = 0; i < readUsernames.Count; i++)
+        {
+            if (readUsernames[i] == username && readPasswords[i] == password)
+            {
+                colourBlindSetting = readColourBlindSettings[i];
+                dyslexicSetting = readDyslexicSettings[i];
+                result = true;
+                break;
+            }
+        }
+
+        return result;
     }
 }
--- UCLanARApp/Assets/Scripts/RegisterHandler.cs
+++ UCLanARApp/Assets/Scripts/RegisterHandler.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,17 +9,36 @@ using UnityEngine.UI;
 public class RegisterHandler : MonoBehaviour
 {
     // SerializeField tag allows things to be seen and assigned in the editor, but keeps variables private
-    [SerializeField] private TMP_InputField nameInput;
-    [SerializeField] private TMP_InputField emailInput;
+    [SerializeField] private TMP_InputField usernameInput;
     [SerializeField] private TMP_InputField passwordInput;
     [SerializeField] private Button registerButton;
     [SerializeField] private TMP_Text errorText;
+    [SerializeField] private TMP_Dropdown colourBlindDropdown;
+    [SerializeField] private TMP_Dropdown dyslexicDropDown;
     [SerializeField] private Button loginButton;
+
+    private List<string> readUsernames = new List<string>();
+    private List<string> readPasswords = new List<string>();
+    private List<int> readColourBlindSettings = new List<int>();
+    private List<bool> readDyslexicSettings = new List<bool>();
 
     void Start()
     {
         registerButton.onClick.AddListener(Register);
         loginButton.onClick.AddListener(Login);
+
+        string filePath = Application.dataPath + "/database.csv";
+        StreamReader reader = new StreamReader(filePath);
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            string[] values = line.Split(',');
+            readUsernames.Add(values[0]);
+            readUsernames.Add(values[1]);
+            readColourBlindSettings.Add(Convert.ToInt32(values[2]));
+            readDyslexicSettings.Add(Convert.ToBoolean(values[3]));
+        }
+        reader.Close();
     }
 
     private void Login()
@@ -26,14 +48,9 @@ public class RegisterHandler : MonoBehaviour
 
     private void Register()
     {
-        if (string.IsNullOrEmpty(nameInput.text))
+        if (string.IsNullOrEmpty(usernameInput.text))
         {
-            errorText.text = "Please enter your name!";
-            errorText.enabled = true;
-        }
-        else if (string.IsNullOrEmpty(emailInput.text))
-        {
-            errorText.text = "Please enter your email address!";
+            errorText.text = "Please enter a username!";
             errorText.enabled = true;
         }
         else if (string.IsNullOrEmpty(passwordInput.text))
@@ -41,9 +58,39 @@ public class RegisterHandler : MonoBehaviour
             errorText.text = "Please enter a password!";
             errorText.enabled = true;
         }
-        else
+        else //all details correct, check username isn't already registered
         {
-            SceneManager.LoadScene("HomeScreen");
+            for (int i = 0; i < readUsernames.Count; i++)
+            {
+                if (readUsernames[i] == usernameInput.text && readPasswords[i] != passwordInput.text) //username already exists, wrong password
+                {
+                    errorText.text = "Username already exists!";
+                    errorText.enabled = true;
+                }
+                else if(readUsernames[i] == usernameInput.text && readPasswords[i] == passwordInput.text) //username already exists and correct password, login
+                {
+                    LoginHandler.colourBlindSetting = readColourBlindSettings[i];
+                    LoginHandler.dyslexicSetting = readDyslexicSettings[i];
+                    SceneManager.LoadScene("HomeScreen");
+                }
+                else
+                {
+                    string filePath = Application.dataPath + "/database.csv";
+                    string colourBlindSetting = "0";
+                    string dyslexicSetting = "FALSE";
+                    StreamWriter writer = new StreamWriter(filePath, true);
+                    colourBlindSetting = colourBlindDropdown.value.ToString();
+                    if (dyslexicDropDown.value == 1)
+                    {
+                        dyslexicSetting = "TRUE";
+                    }
+                    string line = usernameInput.text + "," + passwordInput.text + "," + colourBlindSetting + "," + dyslexicSetting;
+                    writer.WriteLine(line);
+                    writer.Close();
+                    SceneManager.LoadScene("HomeScreen");
+                    break;
+                }
+            }
         }
     }
 }
